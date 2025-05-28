e04e54e21e7a434263a47e8a24e2434b800ebf60
Sahil Chetankumar Patel <s4064014@student.rmit.edu.au>
2025-05-28 20:39:43 +1000

    intial checking for commits

---
+++ src/main/java/au/edu/rmit/sct/App.java
@@ -0,0 +1,13 @@
+package au.edu.rmit.sct;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
